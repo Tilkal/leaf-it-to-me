5875396b546038e4fdce57ac12b854b624d26929
Jonathan Brayette <jonathan.brayette@tilkal.com>
2025-01-27 15:46:00 +0100

    feat: adds switch input for boolean

--- lib/components/Leaf.tsx
+++ lib/components/Leaf.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useMemo, useState } from 'react'
 import { Leaf as LeafProps, LeafType } from '../defs'
 import { classNames } from '../utils/classNames'
 import { isValidNumber, isValidString } from '../utils/json'
+import { Switch } from './Switch'
 import { TypeSelector } from './TypeSelector'
 import { TypeTag } from './TypeTag'
 import { Tick } from './icons/Tick'
@@ -19,6 +20,9 @@ export const Leaf: React.FC<LeafProps> = ({
   const [isEditing, setIsEditing] = useState<boolean>(false)
   const [name, setName] = useState<string>(initName)
   const [value, setValue] = useState<string>(initValue?.toString() ?? '')
+  const [isChecked, setIsChecked] = useState<boolean>(
+    typeof initValue === 'boolean' ? initValue : false,
+  )
   const [type, setType] = useState<LeafType>(initType)
   const [errors, setErrors] = useState<Record<string, boolean>>({})
 
@@ -36,9 +40,11 @@ export const Leaf: React.FC<LeafProps> = ({
       case 'string':
         setErrors((prev) => ({ ...prev, value: !isValidString(value) }))
         break
-
       case 'number':
-        setErrors((prev) => ({...prev, value: !isValidNumber(value)}))
+        setErrors((prev) => ({ ...prev, value: !isValidNumber(value) }))
+        break
+      case 'boolean':
+        setErrors((prev) => ({ ...prev, value: false }))
         break
       default:
         break
@@ -74,14 +80,22 @@ export const Leaf: React.FC<LeafProps> = ({
             value={name}
             onChange={(event) => setName(event.target.value)}
           />
-          <input
-            className={classNames('leaf-input', 'input-value', {
-              error: errors.value,
-            })}
-            type="text"
-            value={value?.toString() ?? ''}
-            onChange={(event) => setValue(event.target.value)}
-          />
+          {['string', 'number'].includes(type) && (
+            <input
+              className={classNames('leaf-input', 'input-value', {
+                error: errors.value,
+              })}
+              type="text"
+              value={value?.toString() ?? ''}
+              onChange={(event) => setValue(event.target.value)}
+            />
+          )}
+          {type === 'boolean' && (
+            <Switch
+              checked={isChecked}
+              onChange={(checked) => setIsChecked(checked)}
+            />
+          )}
           <button
             className={classNames('leaf-input-group-button', 'button-submit', {
               error: hasError,
